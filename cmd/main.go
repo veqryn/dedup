475c2fa2bfb5a9ed5e2b14b2cd94e7253c65c6ab
Chris Duncan <veqryn@hotmail.com>
2022-04-07 17:42:40 -0600

    Allow file globs for input files

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 
@@ -30,9 +31,9 @@ func (i *arrayFlags) Set(value string) error {
 
 func main() {
 	// Flags
-	var inFileLoc arrayFlags
+	var inFileGlobs arrayFlags
 	var skipPatterns arrayFlags
-	flag.Var(&inFileLoc, "in", "input file location (flag can be used multiple times)")
+	flag.Var(&inFileGlobs, "in", "input file location or glob (flag can be used multiple times)")
 	flag.Var(&skipPatterns, "skip-pattern", "re2 regex pattern that will skip the line if it matches (flag can be used multiple times)")
 	outFileLoc := flag.String("out", "", "output file location")
 	tmpFileBytes := flag.Uint64("tmp-file-bytes", 250000000,
@@ -40,7 +41,7 @@ func main() {
 	appendFlag := flag.Bool("append", false, "should append to file (default: only allow new files)")
 	flag.Parse()
 
-	if inFileLoc == nil || len(inFileLoc) == 0 {
+	if inFileGlobs == nil || len(inFileGlobs) == 0 {
 		log.Fatal("in flag must be non-empty or omitted for the default")
 	}
 	if outFileLoc == nil || *outFileLoc == "" {
@@ -60,28 +61,6 @@ func main() {
 		skipPatternsCompiled = append(skipPatternsCompiled, re2)
 	}
 
-	// Open input file for reading
-	var inFiles []io.Reader
-	var progressFiles []io.Reader
-	for _, fileLoc := range inFileLoc {
-		inFile, err := os.Open(fileLoc)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer inFile.Close()
-		inFiles = append(inFiles, inFile)
-
-		// Open input file again to track progress
-		progressFile, err := os.Open(fileLoc)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer progressFile.Close()
-		progressFiles = append(progressFiles, progressFile)
-	}
-	inReader := io.MultiReader(inFiles...)
-	progressReader := io.MultiReader(progressFiles...)
-
 	// Create output file for writing
 	var fileOpts int
 	if appendFlag != nil && *appendFlag {
@@ -94,6 +73,39 @@ func main() {
 		log.Fatal(err)
 	}
 	defer outFile.Close()
+
+	// Open input file for reading
+	var inFiles []io.Reader
+	var progressFiles []io.Reader
+	for _, fileGlob := range inFileGlobs {
+		filePaths, err := filepath.Glob(fileGlob)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(filePaths) == 0 {
+			log.Fatalf("No files found: %s", fileGlob)
+		}
+
+		for _, fileLoc := range filePaths {
+			log.Printf("Opening file: %s\n", fileLoc)
+			inFile, err := os.Open(fileLoc)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer inFile.Close()
+			inFiles = append(inFiles, inFile)
+
+			// Open input file again to track progress
+			progressFile, err := os.Open(fileLoc)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer progressFile.Close()
+			progressFiles = append(progressFiles, progressFile)
+		}
+	}
+	inReader := io.MultiReader(inFiles...)
+	progressReader := io.MultiReader(progressFiles...)
 
 	// Dedup
 	log.Println("Starting dedup...")
